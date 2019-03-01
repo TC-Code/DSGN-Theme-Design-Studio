7d4002f918a014a99e6cd098e5291019485963f8
Tomasz Chowaniec <tomasz.chowaniec@icloud.com>
2019-03-01 20:10:32 +0100

    Add basic scructure Studio, Staff, News and Footer sections

--- js/script.js
+++ js/script.js
@@ -31,7 +31,28 @@ const linkClick = document.querySelectorAll('.hide a');
   })
 })
 
-// 
 let rowHeight = document.querySelector('.header').clientHeight;
 
 console.log(rowHeight);
+
+// Google Maps
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {
+    lat: -118.1893341,
+    lng: 33.7652036
+  };
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+    document.getElementById('map'), {
+      zoom: 4,
+      center: uluru
+    });
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
